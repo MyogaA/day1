48001bd5249dc288de0611dd8e0a490a7508d5ef
M Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-07-03 17:46:52 +0700

    updatetaskday3

---
+++ b48/taskday3/index.js
@@ -0,0 +1,46 @@
+function isValidEmail(email) {
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return emailRegex.test(email);
+  }
+
+  function isValidPhoneNumber(phoneNumber) {
+    const phoneRegex = /^\d{10,14}$/; 
+    return phoneRegex.test(phoneNumber);
+  }
+
+  function validateAndSend() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phone').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    if (name.trim() === '') {
+      alert('Please enter your name.');
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+    if (!isValidPhoneNumber(phoneNumber)) {
+      alert('Please enter a valid phone number.');
+      return;
+    }
+
+    if (subject.trim() === '') {
+      alert('Please enter the subject.');
+      return;
+    }
+
+    if (message.trim() === '') {
+      alert('Please enter your message.');
+      return;
+    }
+
+    let a = document.createElement('a')
+  a.href = `mailto:${email}?subject=${subject}&body=Halo nama saya ${name},\n${message}, silahkan kontak saya di nomor berikut : ${phone}`
+  a.click()
+  }
